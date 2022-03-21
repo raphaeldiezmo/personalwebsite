8dc138267949742b8c4b7162f79a1c671044e156
raphael-di-ezmo <80466011+raphael-di-ezmo@users.noreply.github.com>
2022-03-21 21:07:05 +0000

    content update

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,40 @@
 // Author: Raphael Di Ezmo
 // Description:
 // - This will be the javascript of my personal Website
-function infoReadMeFunction() {
-  var moreText = document.getElementById("moreInformation");
-  var btnText = document.getElementById("infoReadMe");
+var body = document.body;
 
-   if (dots.style.display === "none") {
-     dots.style.display = "inline";
-     btnText.innerHTML = "Read more";
-     moreText.style.display = "none";
-   } else {
-     dots.style.display = "none";
-     btnText.innerHTML = "Read less";
-     moreText.style.display = "inline";
-   }
+console.log("JS loaded");
+function popup_menu(){
+  var menu_button = document.getElementById("menu-bttn");
+  var menu = document.getElementById("menu");
+
+  document.getElementById("menu-display").style.display = "block";
+  console.log("hello world")
+  disableScroll();
+
+  document.getElementById("full-page-container").style.cssText = "filter: blur(8px);  -webkit-filter: blur(8px);"
+
+
+
+
+
+}
+
+function disableScroll() {
+  body.style.overflow = "hidden";
+}
+
+function get_CV(){
+  var cv_button = document.getElementById("cv-bttn");
+  console.log("fuck you")
+
+}
+
+function close_menu(){
+  var menu = document.querySelector("#menu-display");
+  menu.style.display = "none";
+  body.style.overflow = "scroll";
+  document.getElementById("full-page-container").style.cssText =
+   "filter: blur(0px);  -webkit-filter: blur(0px);"
+
 }
