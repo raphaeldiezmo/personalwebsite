72d51b0f0bbd3e799efe5160d1ff5fa6e8457b6e
raphael-di-ezmo <80466011+raphael-di-ezmo@users.noreply.github.com>
2022-03-28 22:15:36 +0100

    changing contents

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,29 @@
 // Author: Raphael Di Ezmo
 // Description:
 // - This will be the javascript of my personal Website
+
 var body = document.body;
 
-console.log("JS loaded");
-function popup_menu(){
-  var menu_button = document.getElementById("menu-bttn");
-  var menu = document.getElementById("menu");
+// console.log("JS loaded");
 
-  document.getElementById("menu-display").style.display = "block";
-  console.log("hello world")
-  disableScroll();
-
-  document.getElementById("full-page-container").style.cssText = "filter: blur(8px);  -webkit-filter: blur(8px);"
-
-
-
-
-
-}
-
+// Disabling scroll
 function disableScroll() {
   body.style.overflow = "hidden";
 }
 
-function get_CV(){
-  var cv_button = document.getElementById("cv-bttn");
-  console.log("fuck you")
+// Opening menu
+function popup_menu(){
+  var menu_button = document.getElementById("menu-bttn");
+  var menu = document.getElementById("menu");
+  document.getElementById("menu-display").style.display = "block";
+  console.log("hello world")
+  disableScroll();
+  document.getElementById("full-page-container").style.cssText = "filter: blur(8px);  -webkit-filter: blur(8px);"
+  document.getElementById("menu-opt-1").style.textAlign = "center";
+  document.getElementById("menu-opt-2").style.textAlign = "center";
 
 }
-
+// Closing menu
 function close_menu(){
   var menu = document.querySelector("#menu-display");
   menu.style.display = "none";
